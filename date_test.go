5fca3f70d4cf530c156b1536c0132913ec8629a3
Philipp Bartsch <tastybug@tastybug.com>
2021-08-26 13:09:19 +0200

    Minor test tweak: use structs to track a variety of test data. Neat!

--- date_test.go
+++ date_test.go
@@ -3,10 +3,17 @@ package main
 import "testing"
 
 func Test_iso8601ToRfc822Date(t *testing.T) {
-	result := ISODateToRSSDateTime(`2020-01-01`)
+	testdata := []struct {
+		input    string
+		expected string
+	}{
+		{input: `2020-01-01`, expected: `Wed, 01 Jan 2020 00:00:00 +0000`},
+		{input: `2020-12-24`, expected: `Thu, 24 Dec 2020 00:00:00 +0000`},
+	}
 
-	expected := `Wed, 01 Jan 2020 00:00:00 +0000`
-	if result != expected {
-		t.Fatalf("Expected: '%s', but got '%s'", expected, result)
+	for _, data := range testdata {
+		if got := ISODateToRSSDateTime(data.input); got != data.expected {
+			t.Fatalf("Expected: '%s', but got '%s'", data.expected, got)
+		}
 	}
 }
